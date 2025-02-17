9e79a221bb68ae38df45443dffe2c3419f6ed843
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-17 17:12:15 +0100

    add FindVowels function

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,9 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function FindVowels(str) {
+    let newWord = ""
+    let vowel = ['a', 'e', 'i', 'o', 'u']
+    for (let i = 0; i < str.length; i++) {
+        const element = str[i];
+        if (element.includes(vowel)) {
+            vowel.push(str)
+        }
+    }
+    return newWord
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+FindVowels(word)
+console.log(FindVowels(word));
 
 
 
