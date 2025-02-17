7021ecfde0086fce47b4b6fe25518455ecf97dc6
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-17 13:52:06 +0100

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,17 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+// Dichiara la funzione qui.
+function HelloName (name) {
+    return 'Ciao ' + name;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+HelloName(userName);
+console.log(HelloName(userName));
+
+
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
